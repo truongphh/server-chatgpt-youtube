70ef542f6e09e1c06a8bd0d4be2f56b5c3117c63
Phạm Hữu Trưởng <truongphh@gmail.com>
2023-02-26 09:20:44 +0700

    #

--- index.js
+++ index.js
@@ -5,44 +5,32 @@ import env from 'dotenv'
 import {Configuration, OpenAIApi} from 'openai'
 
 const app = express()
-
 env.config()
-
 app.use(cors())
 app.use(bodyParser.json())
 
-
-// Configure open api
 const configuration = new Configuration({
     organization: process.env.ORGANIZATION,
-    apiKey: process.env.API_KEY // VISIT .env AND MAKE CHANGES
+    apiKey: process.env.API_KEY
 })
 const openai = new OpenAIApi(configuration)
 
-
-// listeninng
 app.listen(process.env.PORT, ()=>console.log(`listening on port ${process.env.PORT}`))
 
-
-// dummy route to test
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
 
-
-//post route for making requests
 app.post('/', async (req, res)=>{
     const {message} = req.body
-
     try{
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${message}`,
-            max_tokens: 100,
+            max_tokens: process.env.MAX_TOKENS,
             temperature: .5
         })
         res.json({message: response.data.choices[0].text})
-
     }catch(e){
         console.log(e)
         res.send(e).status(400)
